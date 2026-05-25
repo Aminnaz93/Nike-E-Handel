9aa74ec4f3349e483c100f1708abb95818ef6e30
aminnaz93 <Aminnazari040@hotmail.com>
2026-05-25 18:09:43 +0200

    rensingar o tillsätter filer

--- Nike-frontend/nike-frontend/src/App.jsx
+++ Nike-frontend/nike-frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 
 function App() {
   
 
   return (
-    <div>
-      <h1>Nike app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
+
   )
 }
 
