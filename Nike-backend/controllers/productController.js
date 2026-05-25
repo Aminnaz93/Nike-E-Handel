a3c43170e955ded65648e3d74ffc63bf3a959521
aminnaz93 <Aminnazari040@hotmail.com>
2026-05-25 11:00:24 +0200

    productController - hämta alla produkter eller hämta produkter efter id

---
+++ Nike-backend/controllers/productController.js
@@ -0,0 +1,37 @@
+const asyncHandler = require("express-async-handler");
+//(Product) = modellen från models/Product.js
+//används för att prata med products-tabellen i databasen
+const Product = require("../models/Product");
+
+
+//Hämta alla produkter
+//Get 
+// /api/products
+const getAllProducts = asyncHandler(async(req, res) => {
+    //denna hämtar alla produkter från databasen
+    const products = await Product.find();
+
+    res.status(200).json(products);
+});
+
+
+//hämta en produkt via id i databasen
+//GET
+// /api/products/:id
+const getProductById = asyncHandler(async(req, res) => {
+    //hämtar produkten med det id som skickas till den url
+    const product = await Product.findById(req.params.id);
+
+    // en check om produkten finns ens. om produkten inte finns
+    if(!product){
+        res.status(400);
+
+        throw new Error("Produkten hittades inte tyvärr kompis....");
+    }
+
+    res.status(200).json(product);
+
+});
+
+
+module.exports = {getAllProducts, getProductById};
