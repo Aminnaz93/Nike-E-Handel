a3697f4f0677765636e8d3b11c80cfd89af7ab90
aminnaz93 <Aminnazari040@hotmail.com>
2026-05-25 10:44:32 +0200

    skapa filer fllr controllers, routes, middleware

---
+++ Nike-backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+//importera senare våra controllers. vem som ska hantera requesten eller responsen.
+
+//importera senare vår validatetoken.
+
+
+
+
